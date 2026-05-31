e3ae29109aafe97069982d99060f91deaec1d9f2
Aman Goswami <aman@nworx.ai>
2026-05-31 13:23:37 +0530

    Starting: CodePilot project

---
+++ src/test/java/com/example/CodePilot/CodePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodePilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
